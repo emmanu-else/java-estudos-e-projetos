5167ab47b6aad81049155894c0b3a2af073a47c4
Emmanuela Alves <Emmanuelafernanda@hotmail.com>
2026-09-03 18:05:47 -0300

    Add FirstProject.java for basic arithmetic operations

---
+++ exercicios/FirstProject.java
@@ -0,0 +1,18 @@
+
+public class FirstProject
+{	
+
+	public static void main(String[] args) {
+
+    double numero1= 17.5;
+    double numero2= 8.125;
+    
+    double soma = numero1 + numero2;
+    double subtracao = numero1 - numero2;
+    
+    System.out.println("soma     :  " +soma);
+    System.out.println("subtracao  :" +subtracao);
+    
+	}
+	
+}
